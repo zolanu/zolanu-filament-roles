95decc96aa8f3324ab54aba629d0ba38803eadd3
ys-grouse <malsawma7777@gmail.com>
2024-03-11 20:50:54 +0530

    feat[update]: create farmer -> add mutation for vc_id and ip column

--- app/Filament/Resources/FarmerResource/Pages/CreateFarmer.php
+++ app/Filament/Resources/FarmerResource/Pages/CreateFarmer.php
@@ -9,4 +9,15 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateFarmer extends CreateRecord
 {
     protected static string $resource = FarmerResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['vc_id'] = auth()->id();
+        $data['ip'] = request()->ip();
+        return $data;
+    }
 }
