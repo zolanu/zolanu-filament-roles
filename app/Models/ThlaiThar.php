43fc6a53444ebe21eb3013baf4a36e50276e3421
ys-grouse <malsawma7777@gmail.com>
2024-03-11 20:47:05 +0530

    feat[update]: thlai thar model, add relations

--- app/Models/ThlaiThar.php
+++ app/Models/ThlaiThar.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class ThlaiThar extends Model
 {
     use HasFactory;
+
+    public function vegetable()
+    {
+        return $this->belongsTo(Vegetable::class);
+    }
+
+    public function thar_zat_unit()
+    {
+        return $this->belongsTo(MeasurementUnit::class, 'thar_zat_unit_id');
+    }
+
+    public function beisei_zat_unit()
+    {
+        return $this->belongsTo(MeasurementUnit::class, 'beisei_zat_unit_id');
+    }
 }
