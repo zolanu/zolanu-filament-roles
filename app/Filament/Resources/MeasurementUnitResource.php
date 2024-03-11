8b8b69b8ed3d653fcac98dbb1ded5c6433b2d2aa
ys-grouse <malsawma7777@gmail.com>
2024-03-11 13:43:02 +0530

    feat[update]: measurement unit resource

--- app/Filament/Resources/MeasurementUnitResource.php
+++ app/Filament/Resources/MeasurementUnitResource.php
@@ -6,9 +6,11 @@ use App\Filament\Resources\MeasurementUnitResource\Pages;
 use App\Filament\Resources\MeasurementUnitResource\RelationManagers;
 use App\Models\MeasurementUnit;
 use Filament\Forms;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -23,7 +25,8 @@ class MeasurementUnitResource extends Resource
     {
         return $form
             ->schema([
-                //
+                TextInput::make('name')->required(),
+                TextInput::make('abbreviation')->required(),
             ]);
     }
 
@@ -31,7 +34,8 @@ class MeasurementUnitResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('name'),
+                TextColumn::make('abbreviation'),
             ])
             ->filters([
                 Tables\Filters\TrashedFilter::make(),
