3b768c440bbdeb82081699d071c0cf3b57ef79ce
ys-grouse <malsawma7777@gmail.com>
2024-03-11 20:48:52 +0530

    feat[update]: farmer resource -> clean code

--- app/Filament/Resources/FarmerResource.php
+++ app/Filament/Resources/FarmerResource.php
@@ -6,10 +6,14 @@ use App\Filament\Resources\FarmerResource\Pages;
 use App\Filament\Resources\FarmerResource\RelationManagers;
 use App\Models\Farmer;
 use Filament\Forms;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Repeater;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -26,25 +30,45 @@ class FarmerResource extends Resource
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('name')
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('phone')
-                    ->tel()
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('house_no')
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('latitude')
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('longitude')
-                    ->maxLength(255),
-                // Forms\Components\TextInput::make('ip')
-                //     ->maxLength(255),
-                Select::make('district_id')
-                    ->relationship('district', 'name'),
-                // ->hiddenOn('create'),
-                // Forms\Components\TextInput::make('village_council_id')
-                //     ->numeric(),
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('phone'),
+
+                Grid::make([])
+                    ->schema([
+                        TextInput::make('house_no'),
+                        TextInput::make('locality'),
+                        Select::make('district_id')
+                            ->default(fn () => auth()->user()->district_id)
+                            ->relationship('district', 'name'),
+                    ])->columns(2),
+
+                Repeater::make('Thlai thar')
+                    ->columnSpanFull()
+                    ->relationship('thlai_thars')
+                    ->schema([
+                        Select::make('vegetable_id')
+                            ->relationship('vegetable', 'name')
+                            ->required()
+                            ->distinct()
+                            ->placeholder('Thlai hming'),
+                        Grid::make(2)
+                            ->schema([
+                                TextInput::make('thar_zat')
+                                    ->numeric(),
+                                Select::make('thar_zat_unit_id')
+                                    ->default(1)
+                                    ->relationship('thar_zat_unit', 'name')
+                            ]),
+                        Grid::make(2)
+                            ->schema([
+                                TextInput::make('beisei_zat')
+                                    ->numeric(),
+                                Select::make('beisei_zat_unit_id')
+                                    ->default(1)
+                                    ->relationship('beisei_zat_unit', 'name')
+                            ])
+                    ])
             ]);
     }
 
@@ -52,33 +76,28 @@ class FarmerResource extends Resource
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('name')
+                TextColumn::make('name')
                     ->searchable(),
-                Tables\Columns\TextColumn::make('phone')
+                TextColumn::make('phone')
                     ->searchable(),
-                Tables\Columns\TextColumn::make('house_no')
+                TextColumn::make('house_no')
                     ->searchable(),
-                Tables\Columns\TextColumn::make('latitude')
+                TextColumn::make('ip')
                     ->searchable(),
-                Tables\Columns\TextColumn::make('longitude')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('ip')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('district_id')
+                TextColumn::make('district.name')
+                    ->sortable(),
+                TextColumn::make('village_council.name')
                     ->numeric()
                     ->sortable(),
-                Tables\Columns\TextColumn::make('village_council_id')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('created_at')
+                TextColumn::make('created_at')
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
-                Tables\Columns\TextColumn::make('updated_at')
+                TextColumn::make('updated_at')
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
-                Tables\Columns\TextColumn::make('deleted_at')
+                TextColumn::make('deleted_at')
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
