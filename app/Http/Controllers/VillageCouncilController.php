a979ce24137d9ff3762bd6b97cef50b24d069477
ys-grouse <malsawma7777@gmail.com>
2024-03-11 12:04:28 +0530

    feat[add]: village council module

---
+++ app/Http/Controllers/VillageCouncilController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreVillageCouncilRequest;
+use App\Http\Requests\UpdateVillageCouncilRequest;
+use App\Models\VillageCouncil;
+
+class VillageCouncilController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreVillageCouncilRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(VillageCouncil $villageCouncil)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(VillageCouncil $villageCouncil)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateVillageCouncilRequest $request, VillageCouncil $villageCouncil)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(VillageCouncil $villageCouncil)
+    {
+        //
+    }
+}
