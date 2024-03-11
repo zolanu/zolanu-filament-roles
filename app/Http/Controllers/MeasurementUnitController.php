b24896e1df45ac9f8a16bc92b060de8a4b9a9c61
ys-grouse <malsawma7777@gmail.com>
2024-03-11 12:09:46 +0530

    feat[add]: measurement unit module

---
+++ app/Http/Controllers/MeasurementUnitController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreMeasurementUnitRequest;
+use App\Http\Requests\UpdateMeasurementUnitRequest;
+use App\Models\MeasurementUnit;
+
+class MeasurementUnitController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreMeasurementUnitRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(MeasurementUnit $measurementUnit)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(MeasurementUnit $measurementUnit)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateMeasurementUnitRequest $request, MeasurementUnit $measurementUnit)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(MeasurementUnit $measurementUnit)
+    {
+        //
+    }
+}
