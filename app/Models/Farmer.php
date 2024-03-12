d7f72086386176ce253766b4c60c01504b8b2a6a
ys-grouse <malsawma7777@gmail.com>
2024-03-12 13:24:37 +0530

    feat[add]: audit for farmer

--- app/Models/Farmer.php
+++ app/Models/Farmer.php
@@ -5,9 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class Farmer extends Model
+class Farmer extends Model implements Auditable
 {
+    use \OwenIt\Auditing\Auditable;
     use HasFactory, SoftDeletes;
 
     public function thlai_thars()
